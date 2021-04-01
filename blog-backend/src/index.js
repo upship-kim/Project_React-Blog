903e0662caf8a5fb8c63119762d02a102add98ae
upship-kim <tkdqook@gmail.com>
2021-04-01 20:33:20 +0900

    setup nodemon

--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -1,9 +1,12 @@
 const Koa = require('koa');
 const app = new Koa();
 
+//koa는 미들웨어의 배열로 구성되어 있다.
 //app.use 를 통해 미들웨어 함수를 어플리케이션에 등록한다.
+// 첫번째 파라미터 ctx : context의 줄임말, 웹 요청과 응답에 관한 정보를 지니고 있음
+// 두번째 파라미터 next : 현재 처리중인 미들웨어의 다음 미들웨어를 호출하는 함수
 
-app.use((ctx, next) => {
+app.use(async (ctx, next) => {
     console.log(ctx.url);
     console.log(ctx.type);
     console.log(1);
@@ -14,7 +17,11 @@ app.use((ctx, next) => {
         ctx.status = 401; //unauth
         return;
     }
-    next();
+
+    //맞으면 next 미들웨어 처리
+    console.log('await before');
+    await next();
+    console.log('await after');
 });
 app.use((ctx, next) => {
     console.log(2);
