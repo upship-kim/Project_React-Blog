60fa3b64843aa8d4e39fa0cd4facd13f9dab7b53
upship-kim <tkdqook@gmail.com>
2021-04-02 23:10:18 +0900

    setup mongoose & esm(can use import/export modules)

--- blog-backend/src/api/test.js
+++
@@ -1,8 +0,0 @@
-const Router = require('koa-router');
-const testApi = new Router();
-
-testApi.get('/coffee', (ctx) => {
-    ctx.body = 'coffee 성공';
-});
-
-module.exports = testApi;
---
+++ blog-backend/src/main.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+
+const Koa = require('koa');
+const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
+const mongoose = require('mongoose');
+
+//비구조화 할당을 통해 process.env 내부 값에 대한 레퍼런스 만들기
+const { PORT, MONGO_URI } = process.env; //root디렉토리에 .env와 연결되고 있음, dotenv 라이브러리를 통해서 연결되는듯
+
+//connect(url, mongo db option): promise type
+mongoose
+    .connect(MONGO_URI, { useNewUrlParser: true, useFindAndModify: false })
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((e) => {
+        console.error(e);
+    });
+
+const api = require('./api');
+
+const app = new Koa();
+const router = new Router();
+
+//router 설정
+router.use('/api', api.routes()); //api 라우트 적용
+
+//router 적용 전에 bodyParser  적용 (:request body에 JSON을 파싱하여 서버에서 사용할 수 있게 해준다 )
+app.use(bodyParser());
+
+//app 인스턴스에 router를 적용
+app.use(router.routes()).use(router.allowedMethods());
+
+//PORT가 지정되어 있지 않다면 4000을 사용
+const port = PORT || 4000;
+
+app.listen(port, () => {
+    console.log('Listening to port %d', port);
+});
