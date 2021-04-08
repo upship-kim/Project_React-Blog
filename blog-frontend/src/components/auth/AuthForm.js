9674f5013039769fa5bd44db2ac213e81ee0085f
upship-kim <tkdqook@gmail.com>
2021-04-08 17:06:30 +0900

    login view component

--- blog-frontend/src/components/auth/AuthForm.js
+++ blog-frontend/src/components/auth/AuthForm.js
@@ -1,14 +1,84 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import palette from '../../lib/styles/palatte';
+import Button from '../common/Button';
 
 /*
     회원가입 또는 로그인 폼을 보여줌
 */
 
-const AuthFormBlock = styled.div``;
+const AuthFormBlock = styled.div`
+    h3 {
+        margin: 0;
+        color: ${palette.gray[8]};
+        margin-bottom: 1rem;
+    }
+    button {
+        margin-top: 1rem;
+    }
+`;
+
+/*
+    스타일링 input
+*/
+const StyledInput = styled.input`
+    font-size: 1rem;
+    border: none;
+    border-bottom: 1px solid ${palette.gray[5]};
+    padding-bottom: 0.5rem;
+    outline: none;
+    width: 100%;
+
+    &:focus {
+        color: $oc-teal-7;
+        border-bottom: 1px solid ${palette.gray[7]};
+    }
+    & + & {
+        margin-top: 1rem;
+    }
+`;
+
+const Footer = styled.div`
+    margin-top: 2rem;
+    text-align: right;
+    a {
+        color: ${palette.gray[6]};
+        text-decoration: underline;
+        &:hover {
+            color: ${palette.gray[9]};
+        }
+    }
+`;
+
+//autoComplete = 자동완성 입력 필드
 
 const AuthForm = () => {
-    return <AuthFormBlock>authForm</AuthFormBlock>;
+    return (
+        <AuthFormBlock>
+            <h3>로그인</h3>
+            <form>
+                <StyledInput
+                    autoComplete="username"
+                    name="username"
+                    placeholder="아이디"
+                />
+                <StyledInput
+                    autoComplete="new-password"
+                    name="password"
+                    placeholder="비밀번호"
+                    type="password"
+                />
+                {/* Button 태그 뒤로 입력하면 props 값으로 넘어감  */}
+                <Button fullWidth cyan>
+                    로그인
+                </Button>
+            </form>
+            <Footer>
+                <Link to="/register">회원가입</Link>
+            </Footer>
+        </AuthFormBlock>
+    );
 };
 
 export default AuthForm;
--- blog-frontend/src/components/common/Button.js
+++ blog-frontend/src/components/common/Button.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import palette from '../../lib/styles/palatte';
 
 const StyledButton = styled.button`
@@ -16,6 +16,23 @@ const StyledButton = styled.button`
     &:hover {
         background: ${palette.gray[6]};
     }
+
+    ${(props) =>
+        props.fullWidth &&
+        css`
+            padding-top: 0.75rem;
+            padding-bottom: 0.75rem;
+            width: 100%;
+            font-size: 1.125rem;
+        `}
+    ${(props) =>
+        props.cyan &&
+        css`
+            background: ${palette.cyan[8]};
+            &:hover {
+                background: ${palette.cyan[6]};
+            }
+        `}
 `;
 
 // Button에 넣는 이유는 다른 컴포넌트에서 자동으로 import 되게끔하려고 ! (styled-components로 만든 컴포넌트는 자동 import가 안됨)
