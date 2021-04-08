7af71fca9f7fedf3e053a43cdcb74e09ddb9b84e
upship-kim <tkdqook@gmail.com>
2021-04-09 02:12:37 +0900

    auth Reducer

--- blog-frontend/src/modules/auth.js
+++ blog-frontend/src/modules/auth.js
@@ -1,19 +1,47 @@
 import React from 'react';
 import { handleActions, createAction } from 'redux-actions';
+import produce from 'immer';
 
 //액션 타입 정의
-const SAMPLE_ACTION = 'auth/SAMPLE_ACTION';
+const CHANGE_FIELD = 'auth/CHANGE_FIELD';
+const INITIALIZE_FORM = 'auth/INITIALIZE_FORM';
 
 //액션 함수 생성
-export const sampleAction = createAction(SAMPLE_ACTION);
+export const changeField = createAction(
+    CHANGE_FIELD,
+    ({ form, key, value }) => ({
+        form, //login, register
+        key, //username, password, passwordConfirm
+        value, //바꾸려는 값
+    }),
+);
+
+export const initializeForm = createAction(INITIALIZE_FORM, (form) => form);
 
 //초기값
-const initialState = {};
+const initialState = {
+    register: {
+        username: '',
+        password: '',
+        passwordConfirm: '',
+    },
+    login: {
+        username: '',
+        password: '',
+    },
+};
 
 //reducer 생성
 const auth = handleActions(
     {
-        [SAMPLE_ACTION]: (state, action) => state,
+        [CHANGE_FIELD]: (state, { payload: { form, key, value } }) =>
+            produce(state, (draft) => {
+                draft[form][key] = value; //예 :state.register.username 을 바꾼다
+            }),
+        [INITIALIZE_FORM]: (state, { payload: { form } }) => ({
+            ...state,
+            [form]: initialState[form],
+        }),
     },
     initialState,
 );
