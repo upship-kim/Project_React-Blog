e0b93a3a3b45c8aa0e85f87f76513efed9104fdf
upship-kim <tkdqook@gmail.com>
2021-04-09 19:05:12 +0900

    solved error

--- blog-frontend/src/lib/api/auth.js
+++ blog-frontend/src/lib/api/auth.js
@@ -2,12 +2,12 @@ import client from '../api/client';
 
 //로그인
 export const login = ({ username, password }) => {
-    client.post('/api/auth/login', { username, password }); //proxy 설정을 localhost 4000으로 했기에 서버로 요청이 들어감
+    return client.post('/api/auth/login', { username, password }); //proxy 설정을 localhost 4000으로 했기에 서버로 요청이 들어감
 };
 
 //회원가입
 export const register = ({ username, password }) => {
-    client.post('/api/auth/register', { username, password });
+    return client.post('/api/auth/register', { username, password });
 };
 
 //로그인 상태 확인
