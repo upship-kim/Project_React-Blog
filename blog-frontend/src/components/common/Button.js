fffe678ddb3fadb0b64c6d5bb894e6487c4408a9
upship-kim <tkdqook@gmail.com>
2021-04-13 14:08:20 +0900

    create Header component

--- blog-frontend/src/components/common/Button.js
+++ blog-frontend/src/components/common/Button.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styled, { css } from 'styled-components';
 import palette from '../../lib/styles/palatte';
 
-const StyledButton = styled.button`
+const ButtonStyle = css`
     border: none;
     border-radius: 4px;
     font-size: 1rem;
@@ -28,15 +29,25 @@ const StyledButton = styled.button`
     ${(props) =>
         props.cyan &&
         css`
-            background: ${palette.cyan[8]};
+            background: ${palette.cyan[6]};
             &:hover {
-                background: ${palette.cyan[6]};
+                background: ${palette.cyan[4]};
             }
         `}
 `;
 
+const StyledButton = styled.button`
+    ${ButtonStyle}// 버튼 CSS 적용
+`;
+
+const StyledLink = styled(Link)`
+    ${ButtonStyle}
+`;
+
 // Button에 넣는 이유는 다른 컴포넌트에서 자동으로 import 되게끔하려고 ! (styled-components로 만든 컴포넌트는 자동 import가 안됨)
 //{...props} : Button 이 받아오는 모든 props를 넣어주겠다란 의미
-const Button = (props) => <StyledButton {...props} />;
+const Button = (props) => {
+    return props.to ? <StyledLink {...props} /> : <StyledButton {...props} />;
+};
 
 export default Button;
---
+++ blog-frontend/src/components/common/Header.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import styled from 'styled-components';
+import Responsive from './Responsive';
+import Button from './Button';
+
+const HeaderBlock = styled.div`
+    position: fixed;
+    width: 100%;
+    background: white;
+    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.08);
+`;
+
+/*
+    Responsive 컴포넌트 를 활용하고 추가 스타일 적용 하여 새로운 컴포넌트 생성
+*/
+
+const Wrapper = styled(Responsive)`
+    height: 4rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    .logo {
+        font-size: 1.125rem;
+        font-weight: 800;
+        letter-spacing: 2px;
+    }
+    .right {
+        display: flex;
+        align-items: center;
+    }
+`;
+
+const UserInfo = styled.div`
+    font-weight: 800;
+    margin-right: 1rem;
+`;
+
+/*
+    헤더가 fixed로 되어있기에 컨텐츠와 겹칠 수 있음, 그러므로 공백을 둠 
+*/
+
+const Spacer = styled.div`
+    height: 4rem;
+`;
+
+const Header = ({ user }) => {
+    return (
+        <>
+            <HeaderBlock>
+                <Wrapper>
+                    <div className="logo">REACTER</div>
+                    {user ? (
+                        <div className="right">
+                            <UserInfo>{user.username}</UserInfo>
+                            <Button>로그아웃</Button>
+                        </div>
+                    ) : (
+                        <div className="rigth">
+                            <Button to="/login">로그인</Button>
+                        </div>
+                    )}
+                </Wrapper>
+            </HeaderBlock>
+            <Spacer />
+        </>
+    );
+};
+
+export default Header;
