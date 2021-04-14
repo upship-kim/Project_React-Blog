18b138cf5d7902d8a5a08c7fb88349692d1bfcfd
upship-kim <tkdqook@gmail.com>
2021-04-14 18:48:45 +0900

    creating post insert button

--- blog-frontend/src/pages/WritePage.js
+++ blog-frontend/src/pages/WritePage.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Responsive from '../components/common/Responsive';
 import Editor from '../components/write/Editor';
 import TagBox from '../components/write/TagBox';
+import WriteActionButton from '../components/write/WriteActionButton';
 
 const WritePage = () => {
     return (
         <Responsive>
             <Editor />
             <TagBox />
+            <WriteActionButton />
         </Responsive>
     );
 };
